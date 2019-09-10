6e29012d7f9284c2c665541e10d4b25f5f4a6467
Shree Murugesan <shree.murugesan311@gmail.com>
2019-09-10 18:28:40 -0400

    Updates

---
+++ precache-manifest.27aedbaf114770cd27a76cb8da25cbf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e7dd080255c6a9c6a369b44e167116",
+    "url": "/jamming-app/index.html"
+  },
+  {
+    "revision": "651c4e76d408ea0019a7",
+    "url": "/jamming-app/static/css/main.afe08f79.chunk.css"
+  },
+  {
+    "revision": "ec88003390578616ee94",
+    "url": "/jamming-app/static/js/2.c91bada0.chunk.js"
+  },
+  {
+    "revision": "651c4e76d408ea0019a7",
+    "url": "/jamming-app/static/js/main.88a0b203.chunk.js"
+  },
+  {
+    "revision": "f0fbff705868d70d25fc",
+    "url": "/jamming-app/static/js/runtime~main.b135e032.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming-app/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
